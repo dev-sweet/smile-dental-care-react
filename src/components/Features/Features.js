72c1f4f0c0e94b8ffd5eae5b3476979453d6b518
dev-sweet <sweetali0520@gmail.com>
2021-10-19 06:07:49 +0600

    features updated

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-
+import { Col, Container, Row } from 'react-bootstrap';
+import './Features.css';
 const Features = () => {
-    return (
-        <div className="features">
-            <Row md={3}>
-                <Col>
-                  <div className="feature">
-                      
-                  </div>
-                </Col>
-            </Row>
-        </div>
-    );
+  const features = [
+    {
+      id: 1,
+      icon: <i className="fas fa-user-md"></i>,
+      title: 'Certified Dentist',
+      desc: 'Lpsum dolor sit amet consectetur lor adipiscing elit convallis urna quisque id malesuada loremoi pellen demi text tesque',
+    },
+    {
+      id: 2,
+      icon: <i className="fas fa-tooth"></i>,
+      title: 'New Technology',
+      desc: 'Lpsum dolor sit amet consectetur lor adipiscing elit convallis urna quisque id malesuada loremoi pellen demi text tesque',
+    },
+    {
+      id: 3,
+      icon: <i className="fas fa-hands"></i>,
+      title: 'Accept Insurance',
+      desc: 'Lpsum dolor sit amet consectetur lor adipiscing elit convallis urna quisque id malesuada loremoi pellen demi text tesque',
+    },
+  ];
+  return (
+    <div className="features">
+      <Container>
+        <Row className="g-5 gy-md-4">
+          {features.map((feature) => (
+            <Col md={6} lg={4}>
+              <div className="feature">
+                <div className="d-flex align-items-center">
+                  <div className="feature-icon">{feature.icon}</div>
+                  <h4>{feature.title}</h4>
+                </div>
+                <p>{feature.desc}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Features;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-
+import Features from '../Features/Features';
 const Home = () => {
-    return (
-        <div>
-            <Banner />
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <Features />
+    </div>
+  );
 };
 
 export default Home;
